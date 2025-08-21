15d6b8563851617556449de364de329b53878d02
iamasink <iamasink@proton.me>
2025-08-21 06:45:34 +0100

    fix kana translit.

--- src/transliterate.py
+++ src/transliterate.py
@@ -1,7 +1,7 @@
 import re
 
 def to_katakana(word):
-	CV_RE = re.compile(r"^([ptksmnlwjh]?)([aeiou])")
+	CV_RE = re.compile(r"^([kstnpmjlw]?)([aiueo])")
 	VOWEL_MAP = {"a":"ア","i":"イ","u":"ウ","e":"エ","o":"オ"}
 
 
@@ -9,42 +9,51 @@ def to_katakana(word):
 	CV_MAP = {
 	"k": {"a":"カ","i":"キ","u":"ク","e":"ケ","o":"コ"},
 	"s": {"a":"サ","i":"シ","u":"ス","e":"セ","o":"ソ"},
-	"t": {"a":"タ","i":"?","u":"トゥ","e":"テ","o":"ト"},
+	"t": {"a":"タ","i":"？","u":"トゥ","e":"テ","o":"ト"},
 	"n": {"a":"ナ","i":"ニ","u":"ヌ","e":"ネ","o":"ノ"},
 	"p": {"a":"パ","i":"ピ","u":"プ","e":"ペ","o":"ポ"},
 	"m": {"a":"マ","i":"ミ","u":"ム","e":"メ","o":"モ"},
-	"j": {"a":"ヤ","i":"?","u":"ユ","e":"イェ","o":"ヨ"},
+	"j": {"a":"ヤ","i":"？","u":"ユ","e":"イェ","o":"ヨ"},
 	"l": {"a":"ラ","i":"リ","u":"ル","e":"レ","o":"ロ"},
-	"w": {"a":"ワ","i":"ウィ","u":"?","e":"ウェ","o":"?"},
+	"w": {"a":"ワ","i":"ウィ","u":"？","e":"ウェ","o":"？"},
 	}
  
 
-	wordf = word.strip().lower()
+	word = word.strip().lower()
 	out = []
 	i = 0
-	Len = len(wordf)
-	while i < Len:
+	len = len(word)
+	while i < len:
+		char = word[i]
+	
+		if char == " ":
+			out.append("　")
+			i += 1
+			continue
+
 		# n
-		if wordf[i] == "n":
+		if char == "n" and (i + 1 == len or word[i + 1] not in "aiueo"):
 			out.append("ン")
 			i += 1
 			continue
 			
-		
-		match = CV_RE.match(wordf[i:])
-		if not match:
-			i += 1
-			continue
-			
-		consonant, vowel = match.group(1), match.group(2)
-		if consonant == "":
-			out.append(VOWEL_MAP[vowel])
-		else:
-			kv = CV_MAP.get(consonant)
-			if kv:
-				out.append(kv.get(vowel))
-			else:
+		# match consonant + vowel
+		match = CV_RE.match(word[i:])
+		if match:
+			consonant, vowel = match.group(1), match.group(2)
+			if consonant == "":
 				out.append(VOWEL_MAP[vowel])
-		i += len(match.group(0))
-
+			else:
+				kv = CV_MAP.get(consonant)
+				if kv:
+					out.append(kv.get(vowel))
+				else:
+					out.append(VOWEL_MAP[vowel])
+			i += len(match.group(0))
+		else:
+			# unknown character, copy
+			out.append(char)
+			i += 1
+	
 	return "".join(out)
+
